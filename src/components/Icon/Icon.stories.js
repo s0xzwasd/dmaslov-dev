9cbae0e2190e19b0422648454f054a06b3fe0062
Daniil Maslov <s0xzwasd@protonmail.com>
2020-03-08 18:36:31 +0300

    feat: complete icon component

---
+++ src/components/Icon/Icon.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from "./Icon";
+
+export default {
+  title: "Icon",
+  component: Icon
+};
+
+// eslint-disable-next-line jsx-a11y/accessible-emoji
+export const IconCustom = () => <Icon label='Rocket'>‍🚀</Icon>;
+
+export const IconDefault = () => <Icon />;
