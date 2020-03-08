76e411e11ba6763debf14e5ae5b9d1e805ae7572
Daniil Maslov <29286703+s0xzwasd@users.noreply.github.com>
2020-03-08 18:53:33 +0300

    feat: add description component (#10)

---
+++ src/components/Description/Description.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Description from "./Description";
+
+export default {
+  title: "Description",
+  component: Description
+};
+
+const styles = {
+  padding: "30px",
+  backgroundColor: "black"
+};
+
+export const WithText = () => (
+  <div style={styles}>
+    <Description>Hello, I'm Description here!</Description>
+  </div>
+);
+
+export const WithoutText = () => (
+  <div style={styles}>
+    <Description></Description>
+  </div>
+);
