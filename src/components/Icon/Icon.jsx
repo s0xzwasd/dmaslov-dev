c3832260e59aef22a6c7cb99fd0b63cb055c9c63
Daniil Maslov <29286703+s0xzwasd@users.noreply.github.com>
2020-03-08 18:37:21 +0300

    feat: complete icon component (#9)

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Icon.module.scss";
+
+const Icon = ({ children, label }) => {
+  return (
+    <span className={styles.icon} aria-label={label} role='img'>
+      {children}
+    </span>
+  );
+};
+
+Icon.propTypes = {
+  children: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+};
+
+Icon.defaultProps = {
+  children: "🌗",
+  label: "Moon in strange phase"
+};
+
+export default Icon;
