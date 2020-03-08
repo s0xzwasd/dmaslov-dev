9fde2bfbdd01d07d5b9efe193ceec5334fb43206
Daniil Maslov <29286703+s0xzwasd@users.noreply.github.com>
2020-03-08 18:21:22 +0300

    feat: add heading size for content (#8)

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Heading.module.scss";
 
-const Heading = ({ children }) => {
-  return <h2 className={styles.heading}>{children}</h2>;
+const Heading = ({ children, size }) => {
+  return <h2 className={`${styles.heading} ${styles[size]}`}>{children}</h2>;
 };
 
 Heading.propTypes = {
-  children: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(["large", "medium"])
 };
 
 Heading.defaultProps = {
-  children: "Heading default"
+  children: "Heading default",
+  size: "large"
 };
 
 export default Heading;
