68cfd83879294e0415dc0c6198c6637b979f1631
Daniil Maslov <s0xzwasd@protonmail.com>
2020-03-09 22:00:33 +0300

    fix: add some fixes and new props to Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 import Icon from "../Icon";
 import Heading from "../Heading";
 import Description from "../Description";
+import Link from "../Link";
 import styles from "./Card.module.scss";
 
-const Card = ({ description, heading, icon, link }) => {
+const Card = ({ color, description, heading, icon, link, linkTitle }) => {
   return (
-    <section className={styles.card}>
+    <section className={`${styles.card} ${styles[color]}`}>
       <Icon>{icon}</Icon>
       <Heading className={styles.heading}>{heading}</Heading>
       <div className={styles.type}>
@@ -16,23 +17,31 @@ const Card = ({ description, heading, icon, link }) => {
       </div>
       <div className={styles.link}>
         <Description>Specs:</Description>
-        <Description className={styles.content}>{link}</Description>
+        <Description className={styles.content}>
+          <Link to={link} title={linkTitle} isBlank>
+            {link}
+          </Link>
+        </Description>
       </div>
     </section>
   );
 };
 
 Card.propTypes = {
+  color: PropTypes.oneOf(["important"]),
   description: PropTypes.string.isRequired,
   heading: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  linkTitle: PropTypes.string
 };
 
 Card.defaultProps = {
   description: "Social and email",
   heading: "Card title",
   icon: "👨‍",
-  link: "https://www.notion.so/s0xzwasd/Resume-76a1d193249145c7822ce455942786f7"
+  link: "https://www.notion.so/s0xzwasd/Resume-76a1d193249145c7822ce455942786f7",
+  linkTitle: ""
 };
 
 export default Card;
