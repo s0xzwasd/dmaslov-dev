c86544c8bbdcede081774fe2c916caa149852fe9
Daniil Maslov <s0xzwasd@protonmail.com>
2020-03-29 22:09:20 +0300

    feat: add new card with dev.to

--- src/data/other.js
+++ src/data/other.js
@@ -2,7 +2,7 @@ const otherData = [
   {
     color: "default",
     heading: "Certificates",
-    subheading: "RESOURCE",
+    subheading: "Resource",
     description: "Images and PDF files",
     icon: "‍🛰️",
     link: "https://www.notion.so/s0xzwasd/Certific-ates-c9c54a-c340f44d-e08e6dd1-1498548995"
@@ -14,6 +14,14 @@ const otherData = [
     description: "Helping other people",
     icon: "‍☄️",
     link: "https://qna.habr.com/user/s0xzwasd"
+  },
+  {
+    color: "accent",
+    heading: "Publisher",
+    subheading: "Articles",
+    description: "Interesting short notes",
+    icon: "‍🤯",
+    link: "https://dev.to/s0xzwasd"
   }
 ];
 
